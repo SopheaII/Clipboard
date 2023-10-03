0659aa475238b4e104276765e7f1cb4f78d2c5ce
SaoSophea <sophea.sao@wbfinance.com.kh>
2023-10-03 11:15:37 +0700

    Initial Commit

---
+++ Test_MenuBar_Mac/Test_MenuBar_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_MenuBar_MacApp.swift
+//  Test_MenuBar_Mac
+//
+//  Created by Sao Sophea on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_MenuBar_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
